ffbf8543ab52791970690a1e68e2992a32438118
wrieddude <59405252+PlanetDestroyyer@users.noreply.github.com>
2024-04-09 20:19:38 +0530

    new

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient, InsertOneResult, ServerApiVersion } = require('mongodb');
+
+// Replace with your actual MongoDB connection URI
+const uri = "mongodb+srv://iampranavnalawade:QVWQCMoLQyBNZyQF@blooddonation.okjlos6.mongodb.net/?retryWrites=true&w=majority&appName=BloodDonation";
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Improved error handling and feedback
+app.post('/donors', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+      }
+    });
+
+    const db = client.db("blooddonation");
+    const collection = db.collection("donors");
+
+    const newDonor = {
+      name: req.body.name,
+      email: req.body.email, 
+      bloodGroup: req.body.blood_type, 
+      contact: req.body.phone,
+      address: req.body.address
+    };
+
+    const result = await collection.insertOne(newDonor);
+    console.log(`Donor data inserted with ID: ${result.insertedId}`);
+    res.send('Donor information submitted successfully!');
+
+  } catch (error) {
+    console.error("Error inserting donor data:", error);
+    res.status(500).send('Error submitting donor information. Please try again later.');  
+  } 
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });  
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
